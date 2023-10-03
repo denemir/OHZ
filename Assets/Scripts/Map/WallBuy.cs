36129a4a12b07975be6305dd1c23c5ff7c828031
Danny <39076480+denemir@users.noreply.github.com>
2023-10-03 01:19:39 -0500

    WallBuys, minor Interactable changes
    Wall buys introduced conceptually, several texture files added.

---
+++ Assets/Scripts/Map/WallBuy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WallBuy : MonoBehaviour
+{
+    //purchasable weapons
+    public Weapon weapon;
+
+    //events
+    public UnityEvent onPurchase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PurchaseWeapon()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    //to be used for testing hit detection and damage values. target displays damage taken when hit with bullet
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
